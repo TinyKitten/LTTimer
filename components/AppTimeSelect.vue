a429e52ba8ffdd5ad1716f88d1ae156e12a1aee6
TinyKitten <catwalk8205@gmail.com>
2018-10-13 23:03:17 +0900

    タイマー作動中は時間変えられないようにした

--- components/AppTimeSelect.vue
+++ components/AppTimeSelect.vue
@@ -1,22 +1,30 @@
 <template>
   <form class="selector">
-    <label @click="setTarget(300)">
-      <input checked name="time" type="radio">
+    <label @click="onClick(300)">
+      <input :disabled="started" checked name="time" type="radio">
       5分
     </label>
-    <label @click="setTarget(600)">
-      <input name="time" type="radio">
+    <label @click="onClick(600)">
+      <input :disabled="started" name="time" type="radio">
       10分
     </label>
   </form>
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
+import { mapMutations, mapGetters } from 'vuex';
 
 export default {
+  computed: {
+    ...mapGetters(['started']),
+  },
   methods: {
     ...mapMutations(['setTarget']),
+      onClick(num) {
+      if (!this.started) {
+        this.setTarget(num);
+      }
+    }
   },
 };
 </script>
